56a17754df88a340f738dccee4d0f8afe0dd820c
Arian <arian.kulmer@web.de>
2021-09-06 19:46:09 +0530

    sm6250-common: Build new AIDL power hal

--- common.mk
+++ common.mk
@@ -348,7 +348,7 @@ PRODUCT_COPY_FILES += \
 
 # Power
 PRODUCT_PACKAGES += \
-    android.hardware.power@1.3-service.xiaomi_sm6250
+    android.hardware.power-service-qti
 
 # Permissions
 PRODUCT_COPY_FILES += \
