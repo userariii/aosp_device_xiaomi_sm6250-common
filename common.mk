43f95ca5a9c23689591e2d13af19ffa089423582
Omkar Chandorkar <gotenksIN@aospa.co>
2022-11-13 19:51:02 +0900

    sm6250-common: overlay: Configure aux camera for Aperture
    Change-Id: I14e9bf9ba71c9d9f4aca04f3b558a1d72e248a60
    
    Signed-off-by: Omkar Chandorkar <gotenksIN@aospa.co>
    Signed-off-by: COSMIC <rayavanindra@gmail.com>

--- common.mk
+++ common.mk
@@ -317,6 +317,10 @@ DEVICE_PACKAGE_OVERLAYS += \
     $(LOCAL_PATH)/overlay \
     $(LOCAL_PATH)/overlay-lineage
 
+PRODUCT_PACKAGES += \
+   ApertureOverlay \
+   ApertureDevOverlay
+
 PRODUCT_ENFORCE_RRO_TARGETS := *
 
 # Partitions
