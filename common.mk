444984dc0a63d54f10e8cdd49f0e9bb7cc0f54a1
userariii <abhattacharjee717@gmail.com>
2023-07-19 21:09:32 +0000

    sm6250-common: Remove Xiaomi Doze

--- common.mk
+++ common.mk
@@ -162,10 +162,6 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     vendor.qti.hardware.dsp@1.0
 
-# Doze
-PRODUCT_PACKAGES += \
-    XiaomiDoze
-
 # Fastbootd
 PRODUCT_PACKAGES += \
     fastbootd
